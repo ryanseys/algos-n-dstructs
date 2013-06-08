f88f54dbb82f0fda710027f8b90ed3d043e1341c
Ryan Seys <ryan@ryanseys.com>
2013-06-08 11:24:51 -0700

    Add more bubble sort tests

--- test/bubblesort-test.js
+++ test/bubblesort-test.js
@@ -4,9 +4,17 @@ var BubbleSorter = ll.BubbleSorter;
 
 describe('BubbleSort', function() {
   describe('sort standard array', function() {
-    it('should return an empty array after initialized', function() {
+    it('should sort a standard set of numbers', function() {
       var b = new BubbleSorter();
       assert.equal(JSON.stringify([1, 2, 3]), JSON.stringify(b.sort([3, 2, 1])));
+    });
+    it('should return [] for sorting []', function() {
+      var b = new BubbleSorter();
+      assert.equal(JSON.stringify([]), JSON.stringify(b.sort([])));
+    });
+    it('should return [1, 1, 2, 3] for sorting [1, 3, 2, 1]', function() {
+      var b = new BubbleSorter();
+      assert.equal(JSON.stringify([1, 1, 2, 3]), JSON.stringify(b.sort([1, 3, 2, 1])));
     });
   });
 });
