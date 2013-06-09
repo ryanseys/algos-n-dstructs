337c081d5f31c4d599a48d3c2ee2ed04e1966ae9
Ryan Seys <ryan@ryanseys.com>
2013-06-09 10:31:09 -0700

    Add more BST tests

--- test/binarysearchtree-test.js
+++ test/binarysearchtree-test.js
@@ -8,5 +8,26 @@ describe('BinarySearchTree', function() {
       var bst = new BinarySearchTree();
       assert.equal(null, bst.root);
     });
+    it('should have root != null after insert', function() {
+      var bst = new BinarySearchTree();
+      bst.insert(1);
+      assert.notEqual(null, bst.root);
+    });
+    it('should have root.value == 1 after insert(1)', function() {
+      var bst = new BinarySearchTree();
+      bst.insert(1);
+      assert.equal(1, bst.root.value);
+    });
+    it('should have root.value == 2 after insert(2)', function() {
+      var bst = new BinarySearchTree();
+      bst.insert(2);
+      assert.equal(2, bst.root.value);
+    });
+    it('should have root.value == 2 after insert(2) insert(1)', function() {
+      var bst = new BinarySearchTree();
+      bst.insert(2);
+      bst.insert(1);
+      assert.equal(2, bst.root.value);
+    });
   });
 });
