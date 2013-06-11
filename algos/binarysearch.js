9578c5ce3e9e7dbf4fde6c2729765aa4785f2219
Ryan Seys <ryan@ryanseys.com>
2013-06-11 12:06:33 -0700

    Add binary search algo

---
+++ algos/binarysearch.js
@@ -0,0 +1,37 @@
+;(function() {
+  function BinarySearcher() {};
+  BinarySearcher.prototype = {
+
+    search: function(arr, value) {
+      var len = arr.length;
+      var min = 0;
+      var max = len - 1;
+      var middle;
+      while(min <= max) {
+        middle = Math.ceil((max + min)/2); // calc midpoint
+        if(arr[middle] == value) return middle;
+        else if(value > arr[middle]) min = middle + 1;
+        else max = middle - 1;
+      }
+      return -1;
+    }
+  };
+  exports.BinarySearcher = BinarySearcher;
+})();
+
+var BinarySearcher = exports.BinarySearcher;
+var bs = new BinarySearcher();
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(bs.search(arr, 0));
+console.log(bs.search(arr, 1));
+console.log(bs.search(arr, 2));
+console.log(bs.search(arr, 3));
+console.log(bs.search(arr, 4));
+console.log(bs.search(arr, 5));
+console.log(bs.search(arr, 6));
+console.log(bs.search(arr, 7));
+console.log(bs.search(arr, 8));
+console.log(bs.search(arr, 9));
+console.log(bs.search(arr, 10));
+console.log(bs.search(arr, 12));
