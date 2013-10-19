16d3482d6bdabe5a0e8c60792872058d108c77d6
Ryan Seys <ryan@ryanseys.com>
2013-10-19 11:31:11 -0400

    Improve documentation for binary search

--- algos/binarysearch.js
+++ algos/binarysearch.js
@@ -1,7 +1,6 @@
 ;(function() {
   function BinarySearcher() {};
   BinarySearcher.prototype = {
-
     search: function(arr, value) {
       var len = arr.length;
       var min = 0;
@@ -9,13 +8,25 @@
       var middle;
       while(min <= max) {
         middle = Math.ceil((max + min)/2); // calc midpoint
-        if(arr[middle] == value) return middle;
-        else if(value > arr[middle]) min = middle + 1;
-        else max = middle - 1;
+        if(arr[middle] == value) {
+          //found it!
+          return middle;
+        }
+        else if(value > arr[middle]) {
+          // +1 because middle index was included in last search
+          // shortening to the right
+          min = middle + 1;
+        }
+        else {
+          // -1 because middle index was included in last search
+          // shortening to the left
+          max = middle - 1;
+        }
       }
-      return -1;
+      return -1; //couldn't find it
     }
   };
+
   exports.BinarySearcher = BinarySearcher;
 })();
 
--- dstructs/binarysearchtree.js
+++ dstructs/binarysearchtree.js
@@ -45,5 +45,5 @@ b.insert(2);
 b.insert(4);
 b.insert(1);
 b.insert(-1);
-console.log(b.root);
+// console.log(b.root);
 
